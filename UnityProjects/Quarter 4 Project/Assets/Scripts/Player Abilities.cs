e9b9a3333395983caabe4e138432873c12624add
TgoodwinUTC <Trystan.goodwin26@tech4maine.org>
2026-04-06 12:45:53 -0400

    Add DestroyPlatform method to handle platform destruction

--- UnityProjects/Quarter 4 Project/Assets/Scripts/Player Abilities.cs
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/Player Abilities.cs
@@ -32,4 +32,8 @@ public class PlayerAbilities : MonoBehaviour
             Destroy(spawned);
         }
     }
+    public void DestroyPlatform()
+    {
+    Destroy(gameObject.CompareTag("Platform"))
+    }
 }
