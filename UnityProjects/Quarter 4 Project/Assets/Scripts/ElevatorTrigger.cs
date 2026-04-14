dc31c9f3f5129d53ce3f0640c6b63970fb1016d4
brodybilodeau26-cmyk <brody.bilodeau26@tech4maine.org>
2026-04-14 08:45:57 -0400

    push

--- UnityProjects/Quarter 4 Project/Assets/Scripts/ElevatorTrigger.cs
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/ElevatorTrigger.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ElevatorTrigger : MonoBehaviour
 {
     public Elevator elevator;
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
