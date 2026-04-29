80dfaf3a5259abf4242ef69f597f127a5a0913e8
CameronGwinn27 <cameron.gwinn27@tech4maine.org>
2026-04-29 09:39:37 -0400

    Animator Stuff

---
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private GameObject player;
+    public Animator anim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerController>().RunActive == true)
+        {
+            anim.SetTrigger("Running");
+        }
+        if (player.GetComponent<PlayerController>().AttackActive == true)
+        {
+            anim.SetTrigger("AttackTrigger");
+        }
+        if (player.GetComponent<PlayerController>().JumpActive == true)
+        {
+            anim.SetTrigger("Jumped");
+        }
+        if (player.GetComponent<PlayerController>().DashActive == true)
+        {
+            anim.SetTrigger("Dashed");
+        }
+        if (player.GetComponent<PlayerController>().CrouchActive == true)
+        {
+            anim.SetTrigger("isRunning");
+        }
+        if (player.GetComponent<PlayerController>().isGrounded == true)
+        {
+            anim.SetTrigger("Hit ground");
+        }
+    }
+}
