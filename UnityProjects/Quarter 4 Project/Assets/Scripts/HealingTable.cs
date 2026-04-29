023f1fa4f8a243a0af898032162ad7d69b5fa799
brodybilodeau26-cmyk <brody.bilodeau26@tech4maine.org>
2026-04-29 09:39:21 -0400

    push

---
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/HealingTable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class HealingTable : MonoBehaviour{
+
+    public GameObject player;
+    // Flag to track if the player is in the trigger zone
+    private bool playerInRange = false;
+
+    private bool canInteract = false;
+    private Health healthScript;
+    void Start()
+    {
+        
+    }
+
+    // Called when another collider enters the trigger zone
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Check if the entering object is the player
+        {
+            canInteract = true;
+            Debug.Log("Press E to interact"); // Or show UI prompt
+        }
+    }
+
+    // Called once per frame while the player is in the trigger zone
+    void Update()
+    {
+        if (canInteract && Keyboard.current.eKey.IsPressed()) // Check for interaction input (e.g., 'E' key)
+        {
+            Interact();
+        }
+    }
+
+    // Called when another collider exits the trigger zone
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = false;
+            Debug.Log("Interaction disabled"); // Or hide UI prompt
+        }
+    }
+    public void Interact()
+    {
+        Debug.Log("Interaction occurred!");
+        // Add your custom interaction code here (e.g., open a door, pick up an item, etc.)
+        // If using UnityEvent:
+        healthScript.Heal(30);
+    }
+}
