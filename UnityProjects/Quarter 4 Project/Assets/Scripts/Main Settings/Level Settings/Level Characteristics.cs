f2e579766fb0a9e8fb565db178543477c9a03f24
TgoodwinUTC <Trystan.goodwin26@tech4maine.org>
2026-04-29 09:33:51 -0400

    aaaaaaaa

--- UnityProjects/Quarter 4 Project/Assets/Scripts/Main Settings/Level Settings/Level Characteristics.cs
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/Main Settings/Level Settings/Level Characteristics.cs
@@ -3,18 +3,77 @@ using UnityEngine;
 public class LevelCharacteristics : MonoBehaviour
 {
     public bool Reversed;
-    public float healthMod;
-    public float damageMod;
-    public float currencyDropRate;
-    public float healRate;
-    void Start()
+    public float healthMod = 1f;
+    public float damageMod = 1f;
+    public float currencyDropRate = 1f;
+    public float healRate = 1f;
+public void ApplyUpgrade(Upgrade up)
+{
+    ApplyStat(up, true);
+
+    if (up.duration > 0)
     {
-        
+        StartCoroutine(RemoveTemporaryUpgrade(up));
+    }
+}
+    public void ApplyStat(Upgrade up, bool apply)
+    {
+        if (Reversed != true)
+        {
+        float val = apply ? up.value : -up.value;
+
+        switch (up.statToModify)
+        {
+            case StatType.healthMod:
+                healthMod += val;
+                break;
+
+            case StatType.damageMod:
+                damageMod += val;
+                break;
+
+            case StatType.currencyDropRate:
+                currencyDropRate += val;
+                break;
+
+            case StatType.healRate:
+                healRate += val;
+                break;
+             
+        }
+        }
+        if (Reversed == true)
+        {
+            float val = apply ? up.value : -up.value;
+
+            switch (up.statToModify)
+            {
+                case StatType.healthMod:
+                    healthMod -= val;
+                    break;
+
+                case StatType.damageMod:
+                    damageMod -= val;
+                    break;
+
+                case StatType.currencyDropRate:
+                    currencyDropRate -= val;
+                    break;
+
+                case StatType.healRate:
+                    healRate -= val;
+                    break;
+
+            }
+        }
+    }
+    public IEnumerator RemoveTemporaryUpgrade(Upgrade up)
+    {
+        yield return new WaitForSeconds(up.duration);
+        ApplyStat(up, false);
+        Debug.Log($"Temporary upgrade {up.name} has expired");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
