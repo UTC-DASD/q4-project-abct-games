2ccf91c0a794742589447a5cfafb972383be71d0
TgoodwinUTC <Trystan.goodwin26@tech4maine.org>
2026-04-29 09:41:06 -0400

    a

--- UnityProjects/Quarter 4 Project/Assets/Scripts/Main Settings/Level Settings/Upgrade.cs
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/Main Settings/Level Settings/Upgrade.cs
@@ -1,10 +1,6 @@
 using UnityEngine;
 
-public enum UpgradeType
-{
-    Good,
-    Bad
-}
+
 
 public enum StatType
 {
@@ -19,8 +15,7 @@ public class Upgrade
 {
     public string name;           // e.g. "+2 casts per round"
     public string description;    // e.g. "Gives you 2 extra casts for the round"
-    public UpgradeType type;      // Good or Bad
-    public StatType statToModify; // Which stat this affects
+         public StatType statToModify; // Which stat this affects
     public float value;           // +2, -1, 0.5, etc.
     public float duration = 0f; // 0 = permanent, >0 = temporary in seconds
 }
