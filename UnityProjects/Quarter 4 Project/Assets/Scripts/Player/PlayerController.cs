c09cf23ac5c5e79e2a974b2ff09fb3ad08631b61
CameronGwinn27 <cameron.gwinn27@tech4maine.org>
2026-05-04 08:23:54 -0400

    StuffNThings

--- UnityProjects/Quarter 4 Project/Assets/Scripts/Player/PlayerController.cs
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 using System.Collections;
 using System.Data.Common;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
+
+    public GameObject levelstats;
     private PlayerController PC;
     public GameObject platformPrefab;
     public float canCreatePlatform = 0;
@@ -39,14 +41,15 @@ public class PlayerController : MonoBehaviour
     public bool canMove = true;
 
 
-    private float Damage;
-    public float BaseDamage;
+    private int Damage;
+    public int BaseDamage;
     public float AttackCooldown;
     public float lastAttackTime = 0f;
     public bool IsAttacking = false;
     private bool canAttack = true;
     public readonly List<NPCHealth> enemiesInRange = new List<NPCHealth>();
     private bool AirAttack = false;
+    private bool EnemyTookDamage = false;
 
 
     public bool RunActive;
@@ -54,6 +57,35 @@ public class PlayerController : MonoBehaviour
     public bool AttackActive;
     public bool DashActive;
     public bool CrouchActive;
+
+
+        //Ability 3 bools
+        public bool CanUseAbility3 = true;
+    private bool A3Magician = false;
+    private bool A3Empress = false;
+    private bool A3Temperence = false;
+    private bool A3Emperor = false;
+    private bool A3Devil = false;
+    private bool A3Lovers = false;
+    private bool A3Star = false;
+    private bool A3Moon = false;
+    private bool A3Sun = false;
+    private bool A3Judgement = false;
+    private bool A3WoF = false;
+
+        //Ability 4 bools
+        public bool CanUseAbility4 = true;
+    private bool A4Magician = false;
+    private bool A4Empress = false;
+    private bool A4Temperence = false;
+    private bool A4Emperor = false;
+    private bool A4Devil = false;
+    private bool A4Lovers = false;
+    private bool A4Star = false;
+    private bool A4Moon = false;
+    private bool A4Sun = false;
+    private bool A4Judgement = false;
+    private bool A4WoF = false;
 
 
 
@@ -67,6 +99,7 @@ public class PlayerController : MonoBehaviour
         startingRotationX = transform.eulerAngles.x;
         startingRotationY = transform.eulerAngles.y;
         timeSinceLastDash = dashCooldown;
+        Damage = (int)math.round(BaseDamage * levelstats.GetComponent<LevelCharacteristics>().damageMod);
     }
     void Update()
     {
@@ -149,6 +182,10 @@ public class PlayerController : MonoBehaviour
         {
             Instantiate(platformPrefab, transform.position + new Vector3(0, -1, 0), Quaternion.identity);
             canMove = false;
+        }
+        if (EnemyTookDamage == true)
+        {
+            StartCoroutine(EnemyReset());
         }
     }
     // Update is called once per frame
@@ -259,20 +296,229 @@ public class PlayerController : MonoBehaviour
             AirAttack = true;
             rb.AddForceY(-30, ForceMode2D.Impulse);
             canAttack = false;
+            yield return new WaitForSeconds(.5f);
             if (canAttack == true)
             {
+                AirAttack = false;
                 yield break;
             }
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(.3f);
             AirAttack = false;
             yield return new WaitForSeconds(AttackCooldown);
             canAttack = true;
         }
         
     }
+
+    // For Basic Attacks
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("EnemyAttacked");
+            NPCHealth enemyHealth = collision.GetComponent<NPCHealth>();
+            if (enemyHealth != null)
+            {
+                if (IsAttacking == true && EnemyTookDamage == false)
+                {
+                    enemyHealth.TakeDamage(Damage);
+                    EnemyTookDamage = true;
+                }
+                if (AirAttack == true && EnemyTookDamage == false)
+                {
+                    EnemyTookDamage = true;
+                    rb.AddForceY(50, ForceMode2D.Impulse);
+                    enemyHealth.TakeDamage(Damage);
+                    canAttack = true;
+                   
+                }
+            }
+    }
+    }
+
+    private System.Collections.IEnumerator EnemyReset()
+    {
+        yield return new WaitForSeconds(.2f);
+        EnemyTookDamage = false;
+    }
+
+    public void Ability1(InputAction.CallbackContext context)
+    {
         
+    }
+
+    public void Ability2(InputAction.CallbackContext context)
+    {
+        
+    }
+
+
+    public void Ability3(InputAction.CallbackContext context)
+    {
+    if (CanUseAbility3 == true)
+        {
+            if (A3Devil == true)
+            {
+            StartCoroutine(Devil());
+            }
+
+            if (A3Emperor == true)
+            {
+            StartCoroutine(Emperor());
+            }
+
+            if (A3Empress == true)
+            {
+            StartCoroutine(Empress());
+            }
+
+            if (A3Judgement == true)
+            {
+            StartCoroutine(Judgement());
+            }
+
+            if (A3Lovers == true)
+            {
+            StartCoroutine(Lovers());
+            }
+
+            if (A3Magician == true)
+            {
+            StartCoroutine(Magician());
+            }
+
+            if (A3Moon == true)
+            {
+            StartCoroutine(Moon());
+            }
+
+            if (A3Star == true)
+            {
+            StartCoroutine(Star());
+            }
+
+            if (A3Sun == true)
+            {
+            StartCoroutine(Sun());
+            }
+
+            if (A3Temperence == true)
+            {
+            StartCoroutine(Temperance());
+            }
+
+            if (A3WoF == true)
+            {
+            StartCoroutine(WheelOfFortune());
+            }
+        }
+    }
+    
+
+    public void Ability4(InputAction.CallbackContext context)
+    {
+        if (CanUseAbility4 == true)
+        {
+            if (A4Devil == true)
+            {
+            StartCoroutine(Devil());
+            }
+
+            if (A4Emperor == true)
+            {
+            StartCoroutine(Emperor());
+            }
+
+            if (A4Empress == true)
+            {
+            StartCoroutine(Empress());
+            }
+
+            if (A4Judgement == true)
+            {
+            StartCoroutine(Judgement());
+            }
+
+            if (A4Lovers == true)
+            {
+            StartCoroutine(Lovers());
+            }
+
+            if (A4Magician == true)
+            {
+            StartCoroutine(Magician());
+            }
+
+            if (A4Moon == true)
+            {
+            StartCoroutine(Moon());
+            }
+
+            if (A4Star == true)
+            {
+            StartCoroutine(Star());
+            }
+
+            if (A4Sun == true)
+            {
+            StartCoroutine(Sun());
+            }
+
+            if (A4Temperence == true)
+            {
+            StartCoroutine(Temperance());
+            }
+
+            if (A4WoF == true)
+            {
+            StartCoroutine(WheelOfFortune());
+            }
+        }
+    }
+
+    private System.Collections.IEnumerator Magician()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Empress()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Temperance()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Emperor()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Devil()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Lovers()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Star()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Moon()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator  Sun()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator Judgement()
+    {
+        yield return new WaitForSeconds (15);
+    }
+    private System.Collections.IEnumerator WheelOfFortune()
+    {
+        yield return new WaitForSeconds (15);
     }
 
 }
