11530f38ac2430dc797eead5846723c50474aebc
brodybilodeau26-cmyk <brody.bilodeau26@tech4maine.org>
2026-05-15 08:58:33 -0400

    push

--- UnityProjects/Quarter 4 Project/Assets/Scripts/Maps/ToShop.cs
+++ UnityProjects/Quarter 4 Project/Assets/Scripts/Maps/ToShop.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+        public bool canInteract = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player")) // Check if the entering object is the player
+        {
+            canInteract = true;
+            Debug.Log("Press E to interact"); // Or show UI prompt
+        }
     }
 
-    // Update is called once per frame
+    // Called once per frame while the player is in the trigger zone
     void Update()
     {
-        
+        if (canInteract && Keyboard.current.eKey.IsPressed()) // Check for interaction input (e.g., 'E' key)
+        {
+            Interact();
+        }
     }
+
+    // Called when another collider exits the trigger zone
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = false;
+            Debug.Log("Interaction disabled"); // Or hide UI prompt
+        }
+    }
+    public void Interact()
+    {
+        Debug.Log("Interaction occurred!");
+        // Add your custom interaction code here (e.g., open a door, pick up an item, etc.)
+        // If using UnityEvent:
+        SceneManager.LoadScene(6);
+    }
+
 }
